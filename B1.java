598f59734c8765ccf4965a8566226b57dcca10b0
Farid Ahmed <130706495+FaridCS@users.noreply.github.com>
2025-09-21 12:38:50 +0600

    B1 Task

---
+++ B1.java
@@ -0,0 +1,68 @@
+// Common interface
+interface CourseComponent {
+    void showDetails();
+}
+
+// Lesson class
+class Lesson implements CourseComponent {
+    private String title;
+    private int duration;       // in minutes
+    private String instructorName;
+
+    public Lesson(String title, int duration, String instructorName) {
+        this.title = title;
+        this.duration = duration;
+        this.instructorName = instructorName;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Lesson: " + title +
+                " | Duration: " + duration + " mins" +
+                " | Instructor: " + instructorName);
+    }
+}
+
+// CourseModule class
+class CourseModule implements CourseComponent {
+    private String moduleName;
+
+    public CourseModule(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Module: " + moduleName);
+    }
+}
+
+// Factory class
+class CourseFactory {
+    public static CourseComponent createComponent(String type, String name, int duration, String instructor) {
+        if (type.equalsIgnoreCase("LESSON")) {
+            return new Lesson(name, duration, instructor);
+        } else if (type.equalsIgnoreCase("MODULE")) {
+            return new CourseModule(name);
+        } else {
+            throw new IllegalArgumentException("Unknown component type: " + type);
+        }
+    }
+}
+
+// Main class
+public class B1 {
+    public static void main(String[] args) {
+        // Create lessons using factory
+        CourseComponent l1 = CourseFactory.createComponent("LESSON", "Intro to Java", 45, "Alice");
+        CourseComponent l2 = CourseFactory.createComponent("LESSON", "OOP Concepts", 60, "Bob");
+
+        // Create module using factory
+        CourseComponent m1 = CourseFactory.createComponent("MODULE", "OOP Module", 0, null);
+
+        // Display details
+        m1.showDetails();
+        l1.showDetails();
+        l2.showDetails();
+    }
+}
